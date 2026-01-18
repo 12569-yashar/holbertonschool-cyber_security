abbe2ebd614f0837beb2bd41c4496482d97db34b
root <you@example.com>
2026-01-18 09:34:19 +0000

    sagray

--- web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
+++ web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
-input=$(echo "$1" | sed 's/{xor}//')
-echo "$input" | base64 -d | perl -pe '$_ ^= "_" x length'
+
+password="$1"
+
+password="${password#'{xor}'}"
+
+decoded_password=$(echo -n "$password" | openssl enc -base64 -d)
+
+output=""
+
+for ((i = 0; i < ${#decoded_password}; i++)); do
+    # Récupère le caractère à la position actuelle
+    char="${decoded_password:$i:1}"
+    # Convertit le caractère en son code ASCII et effectue l'opération XOR avec 95
+    xor_result=$(( $(printf "%d" "'$char") ^ 95 ))
+    # Ajoute le résultat à la variable de sortie
+    output+=$(printf "\\$(printf '%03o' $xor_result)")
+done
+
+echo "$output"
